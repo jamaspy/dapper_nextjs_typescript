7a46320660722ef687667574df473ce103e9ecb2
jamaspy <aspin@me.com>
2021-09-20 17:22:22 +1000

    fix:api route

--- components/HitCounter.tsx
+++ components/HitCounter.tsx
@@ -10,7 +10,7 @@ const HitCounter = ({ params }: IProps) => {
       return;
     }
     const res = await fetch(
-      `http://localhost:3000/api/register-hit?slug=${params.slug}`
+      `https://nextdapperts.netlify.app/api/register-hit?slug=${params.slug}`
     );
     const data = await res.json();
     return data;
